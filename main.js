0d1f637af978bcfe029b03efdb68324e2083a95e
Anastasia <user@MacBook-Pro-User.local>
2019-11-13 00:15:36 +0300

    have most done

---
+++ main.js
@@ -0,0 +1,30 @@
+$(".slider").slick({
+    nextArrow: $(".next"),
+    adaptiveHeight: true
+  });
+
+  $('.next').on('click', () => {
+      $('.next').css('display', 'none');
+      $('.card').css('opacity', '1');
+      $('.card:first-of-type').addClass('slide-first');
+      $('.card:nth-of-type(2)').addClass('slide-second');
+  })
+
+
+  $(window).scroll(function() {
+
+    $('.on-slide').each(function(){
+
+        var imagePos = $(this).offset().top;
+
+        var topOfWindow = $(window).scrollTop();
+
+        if (imagePos < topOfWindow+400) {
+            $(this).addClass("card-slide");
+        }
+
+    });
+
+});
+
+$('.on-slide').css('visibility', 'hidden');
