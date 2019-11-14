32e0a1b332a428e0b29811253a067d551e00914f
Anastasia <user@MacBook-Pro-User.local>
2019-11-14 16:35:48 +0300

    have final project before refactoring

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let nextClicked = false;
+let click = false;
 
 $(".slider").slick({
     nextArrow: $(".next"),
@@ -15,9 +16,17 @@ $(".slider").slick({
 
   $('.next').on('click', () => {
       if (!nextClicked) {
+        $('.card').each(function(){
+          if (viewportWidth() >= 1024) {
+            $(this).addClass("card-slide");
+          }
+        });
       $('.card').css('opacity', '1');
-      $('.card:first-of-type').addClass('slide-first');
-      $('.card:nth-of-type(2)').addClass('slide-second');
+      if (viewportWidth() < 1024) {
+        $('.card:first-of-type').addClass('slide-first');
+        $('.card:nth-of-type(2)').addClass('slide-second');
+        click = true;
+      }
     } if (nextClicked) {
         $('.prev').css('display', 'block');
         $('.next').css('display', 'none');
@@ -31,6 +40,7 @@ $(".slider").slick({
   $('.prev').on('click', () => {
     $('.card').css('opacity', '1');
     $('.next').css('display', 'block');
+    $('.next').css('animation-delay', '1s');
     $('.prev').css('display', 'none');
     $('.contact h1').removeClass('slide-contact');
     $('.icon').removeClass('show-icon');
@@ -42,15 +52,29 @@ $(".slider").slick({
 
     $('.on-slide').each(function(){
 
-        var imagePos = $(this).offset().top;
+        if (viewportWidth() < 768) {
+          var imagePos = $(this).offset().top;
 
-        var topOfWindow = $(window).scrollTop();
+          var topOfWindow = $(window).scrollTop();
 
-        if (imagePos < topOfWindow+400) {
+          if (imagePos < topOfWindow+400) {
             $(this).addClass("card-slide");
+        } 
+
+        }
+
+        if (viewportWidth() < 1024) {
+          var imagePos = $(this).offset().top - 100;
+
+          var topOfWindow = $(window).scrollTop();
+
+          if (imagePos < topOfWindow+400) {
+            $(this).addClass("card-slide");
+        } 
         }
 
     });
+
     if ($('.card:nth-of-type(7)').hasClass('card-slide')) {
         nextClicked = true;
     }
@@ -59,4 +83,28 @@ $(".slider").slick({
 $('.on-slide').css('visibility', 'hidden');
 
 
+
+
+
+function viewportWidth () {
+  if (window.innerWidth) return window.innerWidth;
+  var
+  doc = document,
+  html = doc && doc.documentElement,
+  body = doc && (doc.body || doc.getElementsByTagName("body")[0]),
+  getWidth = function (elm) {
+    if (!elm) return 0;
+    var setOverflow = function (style, value) {
+      var oldValue = style.overflow;
+      style.overflow = value;
+      return oldValue || "";
+    }, style = elm.style, oldValue = setOverflow(style, "hidden"), width = elm.clientWidth || 0;
+    setOverflow(style, oldValue);
+    return width;
+  };
+  return Math.max(
+    getWidth(html),
+    getWidth(body)
+  );
+}
 
